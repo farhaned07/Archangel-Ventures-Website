6b18e0e15dc19a87d20c3781c69aeb489b889e63
Farhan Sabbir <167243382+farhaned07@users.noreply.github.com>
2026-09-16 01:46:47 +0700

    Expose AI transformation sitemap to crawlers

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: `${site.url}/sitemap.xml`,
+    sitemap: [`${site.url}/sitemap.xml`, `${site.url}/sitemap-ai.xml`],
     host: site.url,
   };
 }
